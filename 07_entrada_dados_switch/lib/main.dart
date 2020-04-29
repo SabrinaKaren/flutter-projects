6f2748251dd4c442417f88c9b60f1e83d876e771
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-04-29 18:08:14 -0300

    App manipulando Switch
    App que exibe mensagem personalizada de acordo com seleção do usuário no switch

---
+++ 07_entrada_dados_switch/lib/main.dart
@@ -0,0 +1,13 @@
+/*
+  Author: Sabrina Karen
+*/
+
+import 'package:flutter/material.dart';
+import 'package:entrada_dados/HomeSwitch.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeSwitch(),
+  ));
+}
