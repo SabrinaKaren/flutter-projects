60162d2dbe3cd6f52feace07ab9189eca432e277
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-04-29 17:18:18 -0300

    App manipulando RadioButton
    App que exibe mensagem de boa-vinda de acordo com o gênero (radio) e nome (texto)

---
+++ 06_entrada_dados_radio/lib/main.dart
@@ -0,0 +1,13 @@
+/*
+  Author: Sabrina Karen
+*/
+
+import 'package:flutter/material.dart';
+import 'package:entrada_dados/HomeRadio.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeRadio(),
+  ));
+}
