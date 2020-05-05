9cd970d437824c9f691cb4e2ad89ef29796e6dcf
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-05-05 12:57:46 -0300

    ** App lista de tarefas

---
+++ lista_de_tarefas/lib/main.dart
@@ -0,0 +1,13 @@
+/*
+  Sabrina Karen
+ */
+
+import 'package:listadetarefas/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
