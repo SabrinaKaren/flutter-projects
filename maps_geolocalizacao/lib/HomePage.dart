e15bb6409c7aeedf9ace7d45211eb05996bf55f3
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-06-04 16:58:58 -0300

    ** Utilizando api de mapa da google

---
+++ maps_geolocalizacao/lib/HomePage.dart
@@ -0,0 +1,37 @@
+/*
+  Sabrina Karen
+ */
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Mapas e geolocalização"),),
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(-23.562436, -46.655005),
+              zoom: 16
+          ),
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+
+}
---
+++ maps_geolocalizacao/lib/main.dart
@@ -0,0 +1,11 @@
+/*
+  Sabrina Karen
+ */
+
+import 'package:flutter/material.dart';
+import 'package:mapsgeolocalizacao/HomePage.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Mapas e geolocalização",
+  home: HomePage(),
+));
