76d95a44ca7671f1867c8a4df754a1c0b7a6ffdd
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-06-14 16:53:15 -0300

    App uber

---
+++ uber/lib/main.dart
@@ -0,0 +1,14 @@
+/*
+  Sabrina Karen
+ */
+
+import 'package:flutter/material.dart';
+import 'package:uber/Pages/HomePage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Uber",
+    home: HomePage(),
+  ));
+}
