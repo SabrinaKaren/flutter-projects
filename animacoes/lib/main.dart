dd4dd7ede00c244e78e40e48383a0ff18ef81f7d
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-06-25 17:39:41 -0300

    Usando animação implícita

---
+++ animacoes/lib/main.dart
@@ -0,0 +1,15 @@
+/*
+  Sabrina Karen
+*/
+
+import 'package:animacoes/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    )
+  );
+}
