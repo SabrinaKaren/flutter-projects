6a99bcdc3e03886e362e46226ff2fe96f365b93f
SabrinaKaren <sabrinaa.karen@hotmail.com>
2020-12-28 15:03:51 -0300

    Update home_controller.dart

--- 34_get_whatsapp/lib/home_page/home_controller.dart
+++ 34_get_whatsapp/lib/home_page/home_controller.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 class HomeController extends ChangeNotifier {
