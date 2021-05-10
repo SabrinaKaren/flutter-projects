44212788c9039f84ab717e6a1b3c0843909fb6c0
Sabrina Karen <sabrinaa.karen@hotmail.com>
2021-05-10 16:21:14 -0300

    mobx_first - subida inicial

---
+++ 36_mobx_first/android/app/src/main/kotlin/br/com/sabrinakaren/mobx_first/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sabrinakaren.mobx_first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 36_mobx_first/lib/main.dart
@@ -0,0 +1,13 @@
+/*
+  Sabrina Karen
+*/
+
+import 'package:flutter/material.dart';
+import 'package:mobx_first/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
